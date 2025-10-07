40c459776c845f8a8619a12b08e5cad38afbbf6d
Beck Bekmyradov <47065940+bekmuradov@users.noreply.github.com>
2025-10-07 18:11:22 +0400

    create is mobile check

---
+++ src/helpers/device.ts
@@ -0,0 +1,6 @@
+/**
+ * Check if user is on mobile device
+ */
+export function isMobile(): boolean {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
